5aaa4d2009f2c041bc74fb7fa59715b030a4e32c
Chris House <chhouse@microsoft.com>
2024-09-27 10:43:14 -0500

    added params, initial deploy script

---
+++ infrastructure/bicep/parameters/main.bicepparam
@@ -0,0 +1,7 @@
+using '../main.bicep'
+
+param workloadName = 'workspacepoc'
+param environmentSuffix = 'dev'
+param location = 'eastus2'
+param apiManagementPublisherName = 'Contoso'
+param apiManagementPublisherEmailAddress = 'apis@contoso.net'
---
+++ infrastructure/scripts/Deploy-Main.ps1
@@ -0,0 +1,23 @@
+# Write a Powershell script that accepts a single parameter, 'ResourceGroup'.  The script should
+# deploy the bicep template named 'main.bicep', located in the ./infrastructure/bicep/ directory to
+# the resource group specified by the 'ResourceGroup' parameter.  The script should use the parameter
+# file named main.bicepparam located in the ./infrastructure/bicep/paramters directory.  The deployment
+# name should be set to a random 6 digit character string based on a guid.
+
+param(
+    [string]$ResourceGroup
+)
+
+$ErrorActionPreference = "Stop"
+
+$scriptPath = $PSScriptRoot
+
+# Deploy the bicep template
+$deploymentName = [guid]::NewGuid().ToString().Substring(0,6)
+
+az deployment group create `
+    --name $deploymentName `
+    --resource-group $ResourceGroup `
+    --template-file "$scriptPath/../bicep/main.bicep" `
+    --parameters "@$scriptPath/../bicep/parameters/main.bicepparam" `
+    --mode Incremental
